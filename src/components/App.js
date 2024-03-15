95d238c98deba08cfe5042e965729630b18e5ada
Debasis Nayak <debasishnayak0739@gmail.com>
2024-03-15 10:46:21 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,85 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import axios from "axios";
+
+const API_KEY = "ed4d31dd300b25a056d846b625caf1b0";
 
 const App = () => {
+  const [query,setQuery] = useState("");
+  const [weatherData,setWeatherData] = useState(null);
+  const [error,setError] = useState(null);
+
+   const handleInputChange = async (e) => {
+    const inputValue = e.target.value;
+    setQuery(inputValue);
+    try{
+        const res = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${API_KEY}`
+        );
+        const fahrenheitTemp = (res.data.main.temp - 273.15) * 9/5 + 32;
+        res.data.main.temp = fahrenheitTemp.toFixed(2);
+       setWeatherData(res.data);
+       setError(null);
+      //  setQuery("");
+    }
+    catch(error){
+      setError("city not found");
+      setWeatherData(null);
+    }
+    // fetch(
+    //   `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${API_KEY}`
+    // )
+    // .then((res) => {
+    //   if(!res.ok){
+    //     throw new Error("City not found");
+    //   }
+    //   return res.json();
+    // })
+    // .then((data) => {
+    //   const fahrenheitTemp = (data.main.temp - 273.15) * 9/5 + 32;
+    //    data.main.temp = fahrenheitTemp.toFixed(2);
+    //   setWeatherData(data);
+    //   setError(null);
+    //   setQuery("");
+    // })
+    // .catch((error) => {
+    //   setError("City not found");
+    //   setWeatherData(null);
+    // });
+     
+   };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <div>
+        <input
+          className="search"
+          onChange={handleInputChange}
+          value={query}
+          type="text"
+          placeholder="Enter a city"
+        />
+      </div>
+      <div className="weather">
+        {weatherData ? (
+          <div>
+            <h2>{weatherData.name}</h2>
+            <h2>{weatherData.main.temp} °F</h2>
+            <p>{weatherData.weather[0].description}</p>
+            <img
+              src={`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
+              alt="weather icon"
+            />
+          </div>
+        ) : error ? (
+          <p>{error}</p>
+        ) : (
+          null
+        )
+        }
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
